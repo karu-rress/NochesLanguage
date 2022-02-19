4763b5043bd6901a3cc97d77371f7407c627a9b9
Karu <nsun527@naver.com>
2022-02-20 00:48:54 +0900

    Add project files.

---
+++ NochesMLCore/NochesML.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Linq.Enumerable;
+
+namespace NochesMLCore
+{
+    public class GuessSender
+    {
+        public string Message { get; set; }
+        public ChatModel.ModelInput Input { get; set; }
+        public ChatModel.ModelOutput Output { get; set; }
+        public string SenderPrediction { get; set; }
+        public List<(string sender, float score)> ScoreWithName { get; set; } = new();
+        public float[] Scores { get; set; }
+        public GuessSender(string msg)
+        {
+            Message = msg;
+            Input = new() { Col1 = msg };
+            Output = ChatModel.Predict(Input);
+
+
+            SenderPrediction = Output.Prediction;
+            Scores = Output.Score;
+            var names = GetSlotNames(ChatModel.PredictEngine.Value.OutputSchema, "Score");
+            foreach (int i in Range(0, Scores.Length))
+            {
+                ScoreWithName.Add((names[i], Scores[i]));
+            }
+        }
+
+        private static List<string> GetSlotNames(DataViewSchema schema, string name)
+        {
+            var column = schema.GetColumnOrNull(name);
+
+            var slotNames = new VBuffer<ReadOnlyMemory<char>>();
+            column.Value.GetSlotNames(ref slotNames);
+            var names = new string[slotNames.Length];
+            var num = 0;
+            foreach (var denseValue in slotNames.DenseValues())
+            {
+                names[num++] = denseValue.ToString();
+            }
+
+            return names.ToList();
+        }
+    }
+
+
+}
---
+++ NochesMLCore/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace NochesMLCore
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleOutputCP(uint wCodePageID);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleCP(uint wCodePageID);
+
+        static void Main(string[] args)
+        {
+            SetConsoleCP(949);
+            SetConsoleOutputCP(949);
+            Console.WriteLine("=====================================");
+            Console.WriteLine("  NOCHES 말투 분석기");
+            Console.WriteLine("=====================================\n");
+            Console.WriteLine("추론하고 싶은 문장을 입력하세요.");
+            Console.WriteLine("exit를 입력하면 종료합니다.\n");
+            string input;
+            GuessSender predict;
+            while (true)
+            {
+                Console.Write(">> ");
+                input = Console.ReadLine();
+
+                if (input == "exit")
+                    return;
+
+                predict = new(input);
+
+                Console.WriteLine("\n=====================================\n");
+                Console.WriteLine($"{predict.SenderPrediction}님이 보냈을 확률이 높습니다.");
+                foreach (var (sender, score) in predict.ScoreWithName)
+                    Console.WriteLine($"{sender}: {score * 100:0.00}%");
+                Console.WriteLine("\n=====================================\n");
+            }
+        }
+    }
+}
